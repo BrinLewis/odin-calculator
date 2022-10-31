f1f0486fa44f3788fb9a36707211d9f19db3b3ab
BrinLewis <brinsley22@hotmail.co.uk>
2022-10-31 15:09:45 +0000

    Fix bug where doing an operation, then operating again on the answer would not work properly

--- javascript.js
+++ javascript.js
@@ -82,19 +82,17 @@ operatorBtns.forEach(btn => {
 const equalsBtn = document.querySelector("#equalsBtn");
 equalsBtn.addEventListener("click", () => {
   let result = operate(operator, storedNum, displayValue);
+  displayValue = result;
   display.textContent = result;
 });
 
-//Create functions "add", "subtract", "multiply" and "divide"
-//Create function "operate" that takes an operator and 2 numbers then calls one of the above functions on the numbers. (Probably use switch statement for each operator case)
-//Create event listeners that populate the display when number buttons are clicked. Store the displayValue in a variable for next steps.
-//Create event listeners for operator buttons that store the current number on the display, clearDisplay, store which operator was chosen.
-//Create event listener for "=" button which runs "operate()" on the stored values.
+//Create functions "add", "subtract", "multiply" and "divide" X
+//Create function "operate" that takes an operator and 2 numbers then calls one of the above functions on the numbers. (Probably use switch statement for each operator case) X
+//Create event listeners that populate the display when number buttons are clicked. Store the displayValue in a variable for next steps. X
+//Create event listeners for operator buttons that store the current number on the display, clearDisplay, store which operator was chosen. X
+//Create event listener for "=" button which runs "operate()" on the stored values. X
 //If there is already a decimal point on the display, don't let the user type another one.
 
 //NOTE: Will need to figure out the proper logic to store the values and call operate() with them.
 
 //Event listener on "CLEAR" that runs a clearDisplay function to set textContent of display to nothing and reset stored values to 0.
-
-//Have a "prevButton" variable which is set to whichever button was pressed last (have it in the event listeners)
-//If prevButton = "=" when a operator button is clicked, run the clearDisplay function first but store prevAnswer in a variable so that when you click "=" next time it will evaluate the operation with prevAnswer as the first number.
